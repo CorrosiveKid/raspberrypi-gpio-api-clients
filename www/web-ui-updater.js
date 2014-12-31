f2f6c8651b0f63df9bd4e6e23a788abf250c993c
Rasika Amaratissa <rasika@embla.asia>
2014-12-31 14:36:53 +0530

    Remote Control Client Application separated from the server code and moved in to a new repo.

---
+++ www/web-ui-updater.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    updateSwitches();
+
+    setInterval(function(){
+        updateSwitches();
+    }, 30000);
+});
+
+function updateSwitches(){
+    var pins = getPinStatus();
+    // var pins = getPinStatusTest();
+    console.log("Updating Switches");
+    pins.forEach(function(pin) {
+        var id = "#flip-select-" + pin.pin_number;
+
+        if (pin.value == 0){
+            $(id).val('Off').slider('refresh');
+        }
+        else{
+            $(id).val('On').slider('refresh');
+        }
+    });
+}
